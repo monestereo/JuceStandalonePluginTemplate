90f47736eaf5d21464639b377d13be0bc18a1d49
marc <monestereo@gmail.com>
2014-08-05 21:53:30 +0200

    initial commit

---
+++ MyPlugin/Source/PluginEditor.cpp
@@ -0,0 +1,112 @@
+/*
+  ==============================================================================
+
+  This is an automatically generated GUI class created by the Introjucer!
+
+  Be careful when adding custom code to these files, as only the code within
+  the "//[xyz]" and "//[/xyz]" sections will be retained when the file is loaded
+  and re-saved.
+
+  Created with Introjucer version: 3.1.0
+
+  ------------------------------------------------------------------------------
+
+  The Introjucer is part of the JUCE library - "Jules' Utility Class Extensions"
+  Copyright 2004-13 by Raw Material Software Ltd.
+
+  ==============================================================================
+*/
+
+//[Headers] You can add your own extra header files here...
+//[/Headers]
+
+#include "PluginEditor.h"
+
+
+//[MiscUserDefs] You can add your own user definitions and misc code here...
+//[/MiscUserDefs]
+
+//==============================================================================
+MyPluginPluginEditor::MyPluginPluginEditor (MyPluginAudioProcessor* ownerFilter)
+    : AudioProcessorEditor(ownerFilter)
+{
+
+    //[UserPreSize]
+    //[/UserPreSize]
+
+    setSize (600, 400);
+
+
+    //[Constructor] You can add your own custom stuff here..
+    startTimer(200);
+    //[/Constructor]
+}
+
+MyPluginPluginEditor::~MyPluginPluginEditor()
+{
+    //[Destructor_pre]. You can add your own custom destruction code here..
+    //[/Destructor_pre]
+
+
+
+    //[Destructor]. You can add your own custom destruction code here..
+    //[/Destructor]
+}
+
+//==============================================================================
+void MyPluginPluginEditor::paint (Graphics& g)
+{
+    //[UserPrePaint] Add your own custom painting code here..
+    //[/UserPrePaint]
+
+    g.fillAll (Colours::white);
+
+    //[UserPaint] Add your own custom painting code here..
+    //[/UserPaint]
+}
+
+void MyPluginPluginEditor::resized()
+{
+    //[UserResized] Add your own custom resize handling here..
+    //[/UserResized]
+}
+
+
+
+//[MiscUserCode] You can add your own definitions of your custom methods or any other code here...
+void MyPluginPluginEditor::timerCallback()
+{
+    MyPluginAudioProcessor* ourProcessor = getProcessor();
+    // float parameter = ourProcessor->getParameter(0);
+
+    // label->setText(juce::String::formatted("%f", test), juce::NotificationType::dontSendNotification);
+
+    // exchange any data you want between UI elements and the Plugin "ourProcessor"
+}
+//[/MiscUserCode]
+
+
+//==============================================================================
+#if 0
+/*  -- Introjucer information section --
+
+    This is where the Introjucer stores the metadata that describe this GUI layout, so
+    make changes in here at your peril!
+
+BEGIN_JUCER_METADATA
+
+<JUCER_COMPONENT documentType="Component" className="MyPluginPluginEditor" componentName=""
+                 parentClasses="public AudioProcessorEditor, public Timer, public ButtonListener"
+                 constructorParams="MyPluginAudioProcessor* ownerFilter" variableInitialisers="AudioProcessorEditor(ownerFilter)"
+                 snapPixels="8" snapActive="1" snapShown="1" overlayOpacity="0.330"
+                 fixedSize="0" initialWidth="600" initialHeight="400">
+  <BACKGROUND backgroundColour="ffffffff"/>
+</JUCER_COMPONENT>
+
+END_JUCER_METADATA
+*/
+#endif
+
+
+//[EndFile] You can add extra defines here...
+//[/EndFile]
---
+++ MyPlugin/Source/PluginEditor.h
@@ -0,0 +1,72 @@
+/*
+  ==============================================================================
+
+  This is an automatically generated GUI class created by the Introjucer!
+
+  Be careful when adding custom code to these files, as only the code within
+  the "//[xyz]" and "//[/xyz]" sections will be retained when the file is loaded
+  and re-saved.
+
+  Created with Introjucer version: 3.1.0
+
+  ------------------------------------------------------------------------------
+
+  The Introjucer is part of the JUCE library - "Jules' Utility Class Extensions"
+  Copyright 2004-13 by Raw Material Software Ltd.
+
+  ==============================================================================
+*/
+
+#ifndef __JUCE_HEADER_5A54073D1A6C6641__
+#define __JUCE_HEADER_5A54073D1A6C6641__
+
+//[Headers]     -- You can add your own extra header files here --
+#include "PluginProcessor.h"
+//[/Headers]
+
+
+
+//==============================================================================
+/**
+                                                                    //[Comments]
+    An auto-generated component, created by the Introjucer.
+
+    Describe your class and how it works here!
+                                                                    //[/Comments]
+*/
+class MyPluginPluginEditor  : public AudioProcessorEditor,
+                                public Timer
+{
+public:
+    //==============================================================================
+    MyPluginPluginEditor (MyPluginAudioProcessor* ownerFilter);
+    ~MyPluginPluginEditor();
+
+    //==============================================================================
+    //[UserMethods]     -- You can add your own custom methods in this section.
+    
+    void timerCallback();
+    MyPluginAudioProcessor* getProcessor() const
+        {return static_cast <MyPluginAudioProcessor*>(getAudioProcessor());}
+    //[/UserMethods]
+
+    void paint (Graphics& g);
+    void resized();
+
+
+
+private:
+    //[UserVariables]   -- You can add your own custom variables in this section.
+    //[/UserVariables]
+
+    //==============================================================================
+
+
+    //==============================================================================
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (MyPluginPluginEditor)
+};
+
+//[EndFile] You can add extra defines here...
+//[/EndFile]
+
+#endif   // __JUCE_HEADER_5A54073D1A6C6641__
